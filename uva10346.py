664d43f6a3971fe05a6ec6d5cd804dc53a01cb1f
alhasib <33780479+alhasib@users.noreply.github.com>
2018-06-02 05:39:59 +0600

    10346 - Peter's Smokes

---
+++ uva10346.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        n,k = list(map(int,input().split()))
+        count = n
+        while(n>k):
+            count += int(n/k)
+            n = int(n/k) + int(n%k)
+        print(count)
+    except EOFError:
+        break
