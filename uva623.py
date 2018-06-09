4e2116d3b3f4be3030e5f6bc3f1975318826ab9b
alhasib <33780479+alhasib@users.noreply.github.com>
2018-06-09 13:44:47 +0600

    623 - 500!

---
+++ uva623.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        n = int(input())
+        r = 1
+        for i in range(1,n+1):
+            r = r*i
+        print("{}!" .format(n))
+        print(r)
+    except EOFError:
+        break
